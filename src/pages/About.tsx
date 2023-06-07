60bf4bdb96c0e0ca1022bf9e6c3712861b8c1869
Alexey Demidov <dr.a.lekter@gmail.com>
2023-06-08 01:01:32 +0400

    Added routing, colors and font

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+    return (
+        <>
+            <h1>About Page</h1>
+            <p>Text for about page.</p>
+        </>
+    );
+};
+
+export default AboutPage;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import DashboardPage from './Dashboard';
+import AboutPage from './About';
+
+export {
+    DashboardPage,
+    AboutPage,
+}
